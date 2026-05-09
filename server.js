13f920a424f6a6099550528d487b4b6f5e5ba606
Sunil Dethariya <sunilahir9830@gmail.com>
2026-05-09 15:31:32 +0530

    fixed mysql pool connection

--- server.js
+++ server.js
@@ -225,11 +225,15 @@ app.use(cors());
 app.use(express.json());
 
 // 🔹 DB Connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
